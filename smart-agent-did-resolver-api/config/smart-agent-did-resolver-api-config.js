a83a645edb4d3f0fb9e94f1c2fd21a4255cde694
Philip Kaiser <phiipkaiser30.05@gmail.com>
2019-12-05 15:16:16 +0100

    Added test account

--- smart-agent-did-resolver-api/config/smart-agent-did-resolver-api-config.js
+++ smart-agent-did-resolver-api/config/smart-agent-did-resolver-api-config.js
@@ -14,3 +14,19 @@ exports['default'] = {
     }
   }
 }
+
+exports['test'] = {
+  ethAccounts: {
+    '0x46610b67Eb8a11bC8BD5EB935F42963BB047e4d5': 'ed4a97f7b55a48bbf93a6e12852f5f625bd3750475538a6c1ac5d9a9ce5a7fc6'
+  },
+  encryptionKeys: {
+    '0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563': '',
+    '0x633dc4d7da7256660a892f8f1604a44b5432649cc8ec5cb3ced4c4e6ac94dd1d': ''
+  },
+  smartAgentDidResolverApi: (api) => {
+    return {
+      disabled: false,
+      ethAccount: '0x46610b67Eb8a11bC8BD5EB935F42963BB047e4d5'
+    }
+  }
+}
